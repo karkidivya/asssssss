448e989edd6d5d01fece836402b22e713568e22f
Bibek Prasad Kafle <111261432+Bibek16@users.noreply.github.com>
2023-07-24 20:36:45 -0400

    Update store-service.js

--- store-service.js
+++ store-service.js
@@ -1,12 +1,13 @@
 /********************************************************************************* 
 
-WEB322 – Assignment 05
+WEB322 – Assignment 02 
 I declare that this assignment is my own work in accordance with Seneca
 Academic Policy.  No part of this assignment has been copied manually or 
 electronically from any other source (including 3rd party web sites) or 
 distributed to other students. I acknoledge that violation of this policy
 to any degree results in a ZERO for this assignment and possible failure of
 the course. 
+
 
 Name:   Bibek Prasad Kafle
 Student ID:   152656211
@@ -26,7 +27,6 @@ var sequelize = new Sequelize("fttopwel", "fttopwel", "qaZeBGuEf4Z2fSzBSyD6yL-g-
   query: { raw: true },
 });
 
-// Define the Item model
 const Item = sequelize.define("item", {
   body: Sequelize.TEXT,
   title: Sequelize.STRING,
@@ -35,22 +35,16 @@ const Item = sequelize.define("item", {
   published: Sequelize.BOOLEAN,
   price: Sequelize.DOUBLE,
 });
-
-// Define the Category model
 const Category = sequelize.define("category", {
   category: Sequelize.STRING,
 });
-
-// Define the relationship: Item belongs to Category
 Item.belongsTo(Category, { foreignKey: "categoryId" });
 
-// Export the models
 module.exports = {
   Item,
   Category,
 };
 
-// Initialize the database
 module.exports.initialize = function () {
   return new Promise((resolve, reject) => {
     sequelize
